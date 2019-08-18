1c5a4c127f3727a876a17816205871b83881353d
Evan Kochuk <35906327+RavenDreamer@users.noreply.github.com>
2019-08-18 17:20:07 -0400

    Re-adding Synchronized Telepad and Wrangle Flyers (Lightbugs only atm)

---
+++ SynchronizedTelepad/SynchronizedTelepadPatches.cs
@@ -0,0 +1,53 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StormShark.OniMods
+{
+	public static class SynchronizedTelepadPatches
+	{
+		public static readonly float twoDays = 1200f;
+		public static readonly float threeDays = 1800f;
+		public static readonly float immigrationTime = 350f;
+		//Game starts cycle 1 at 50, with .5 cycles (aka 300) until first print.
+		//All subsequent prints aree 1800 after.
+		//This means that we want to return between 2 and 3 cycles @ 350, daily time
+
+
+		[HarmonyPatch(typeof(Immigration), MethodType.Normal)]
+		[HarmonyPatch("EndImmigration")]
+		public static class Immigration_Patch_a
+		{
+			public static void Postfix(Immigration __instance)
+			{
+				__instance.timeBeforeSpawn = SynchronizedTelepadPatches.GetTimeUntill350();
+			}
+		}
+
+		[HarmonyPatch(typeof(Immigration), MethodType.Normal)]
+		[HarmonyPatch("Stop")]
+		public static class Immigration_Patch_b
+		{
+			public static void Postfix(Immigration __instance)
+			{
+				__instance.timeBeforeSpawn = SynchronizedTelepadPatches.GetTimeUntill350();
+			}
+		}
+
+		public static float GetTimeUntill350()
+		{
+			float sinceStartOfCycle = GameClock.Instance.GetTimeSinceStartOfCycle();
+			if ((double)sinceStartOfCycle <= (double)immigrationTime)
+			{
+				return twoDays + (immigrationTime - sinceStartOfCycle);
+			}
+			else
+			{
+				return threeDays - (sinceStartOfCycle - immigrationTime);
+			}
+		}
+
+	}
+}
---
+++ WrangleFlyers/WrangleFlyersPatches.cs
@@ -0,0 +1,22 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace StormShark.OniMods
+{
+	public static class WrangleLightBugPatches
+	{
+		[HarmonyPatch(typeof(BaseLightBugConfig), MethodType.Normal)]
+		[HarmonyPatch("BaseLightBug")]
+		public static class WrangleLightBug_Patch
+		{
+			public static void Postfix(GameObject __result)
+			{
+				__result.AddOrGet<Capturable>().allowCapture = true;
+			}
+		}
+	}
+}
